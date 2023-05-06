c7e88bf9539058b2cfb3157b69c22d9c6bfb0ae8
Pavel <pavel.chernenko97@gmail.com>
2023-05-06 23:58:52 +0300

    feat: add expense list and item widgets

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../../models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 20,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              expense.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              children: [
+                Text(
+                  '\$${expense.amount.toStringAsFixed(2)}',
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(
+                      categoryIcons[expense.category],
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      expense.formattedDate,
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
