e300500a05ad5826369fe66eb4fe60dc0de1af68
Pavel <pavel.chernenko97@gmail.com>
2023-05-06 23:59:12 +0300

    feat: add expense model

---
+++ lib/models/expense.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+final formatter = DateFormat.yMd();
+
+enum Category {
+  food,
+  work,
+  leisure,
+  travel,
+}
+
+const categoryIcons = {
+  Category.food: Icons.food_bank,
+  Category.leisure: Icons.movie_creation_outlined,
+  Category.work: Icons.work,
+  Category.travel: Icons.airplane_ticket_outlined,
+};
+
+class Expense {
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
